158223881b80e7485d948f8e3a509a4fa2962906
Stanley <mraaa711128@gmail.com>
2014-11-06 03:32:19 -0500

    Initial Commit

---
+++ VAShop/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  VAShop
+//
+//  Created by Stanley on 11/6/14.
+//  Copyright (c) 2014 Stanley. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
